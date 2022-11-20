e3dc21d6aa6aaf74d366678ddd66c5d6432354fa
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-11-20 21:04:17 +0100

    #183820192 Modifify accordian component

--- components/Accordion/Accordion.tsx
+++ components/Accordion/Accordion.tsx
@@ -7,7 +7,7 @@ type optionProps = {
   content: string;
 };
 const AccordionLayout = ({ options }: any) => {
-  const [isActive, setIsActive] = useState(null);
+  const [activeTab, setActiveTab] = useState(null);
 
   return (
     <>
@@ -17,12 +17,12 @@ const AccordionLayout = ({ options }: any) => {
           className="overflow-hidden flex w-full flex-col my-3 border-2 rounded-md border-secondary-300 hover:border-primary-300"
         >
           <div
-            onClick={() => setIsActive(isActive === index ? null : index)}
+            onClick={() => setActiveTab(activeTab === index ? null : index)}
             className="flex items-center p-5 lg:p-6 justify-between  cursor-pointer bg-secondary-25 hover:bg-white "
           >
             <div className="flex items-center gap-4 text-secondary-600 font-bold">
               <span>
-                {isActive === index ? (
+                {activeTab === index ? (
                   <ArrowRight2
                     size="16"
                     className="rotate-90"
@@ -35,14 +35,14 @@ const AccordionLayout = ({ options }: any) => {
               </span>
               <p
                 className={classNames(" font-bold text-sm md:text-base", {
-                  "text-primary-500": isActive === index,
+                  "text-primary-500": activeTab === index,
                 })}
               >
                 {title}
               </p>
             </div>
           </div>
-          {index === isActive && (
+          {index === activeTab && (
             <div className=" w-full leading-6 text-secondary-600  transition-height ease duration-500 p-4 px-10 mb-6">
               {content}
             </div>
